cd9db41b00920823cb8fd72cd3458e015d2ea97a
Didoramadhan <didoramadhan3@gmail.com>
2024-09-10 09:14:01 +0700

    add

---
+++ build_push_image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker build -t item-app:v1
+docker images
+docker tag item-app:v1 ghcr.io/didatzy/item-app:v1
+echo $githubtoken| docker login ghcr.io -u didatzy --password-stdin
+docker push ghcr.io/didatzy/item-app:v1
