99b66cf094e383389b7e3c69cf708e8dce7296e4
DidaTzy <fannygaming805@gmail.com>
2024-09-10 09:54:25 +0700

    add

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,5 +2,5 @@
 docker build -t item-app:v1 .
 docker images
 docker tag item-app:v1 ghcr.io/didatzy/item-app:v1
-echo $githubtoken| docker login ghcr.io -u didatzy --password-stdin
+echo $git | docker login ghcr.io -u didatzy --password-stdin
 docker push ghcr.io/didatzy/item-app:v1
